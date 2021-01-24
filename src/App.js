180767dd5836e3de0f086db8b659d1312f50ece2
Annamari Aalto <annamari.aalto@hotmail.com>
2021-01-24 16:49:27 +0200

    Complete NameApplication
    Complete NameApplication.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,102 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Namelist from "./names.json";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello world!
-        </p>
-        <p>
-          Hello is it me you're looking for?
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+//Parent component
+const App = (props) => {
+   return (
+    <div>
+      <div className="App">     
+        <Raw persons={Namelist.names}/>
+        <hr></hr>
+        <Popularity persons={Namelist.names}/>
+        <hr></hr>
+        <Alphabethically persons={Namelist.names}/>
+        <hr></hr>
+        <Total persons={Namelist.names}/>
+        <hr></hr>
+        <Length persons={Namelist.names}/>
+        <hr></hr>
+      </div>
     </div>
-  );
+   );
+}
+
+//Child component 1 - Display raw data
+const Raw = (props) => {
+  return (    
+    <div className="Raw">
+      <h3>Display raw data</h3>
+      {props.persons.map((item, i) => (
+        <div key={i} style={{margin: '30px'}}>
+        <div>{`Name: ${item.name}`}</div>
+        <div>{`Amount: ${item.amount}`}</div>
+        </div>      
+      ))
+      }
+    </div>
+  )
+}
+
+//Child component 2 - List names and amounts, order by amount, most popular first
+const Popularity = (props) => {
+  return (    
+    <div className="Popularity">
+      <h3>Display by popularity</h3>
+      {props.persons.sort((a, b) => a.amount > b.amount ? -1 : 1 )
+      .map((item, i) => (
+        <div key={i} style={{margin: '30px'}}>
+        <div>{`Name: ${item.name}`}</div>
+        <div>{`Amount: ${item.amount}`}</div>
+        </div>  
+      ))
+      } 
+    </div>
+  )
+}
+
+// Child component 3 - List names in alphabetical order
+const Alphabethically = (props) => {
+  return (
+    <div className="Alphabethically">
+      <h3>Display Alphabethically</h3>
+      {props.persons.sort((a, b) => a.name > b.name ? 1 : -1)
+      .map((item, i) => (
+        <div key={i} style={{margin: '30px'}}>
+        <div>{`Name: ${item.name}`}</div>
+        <div>{`Amount: ${item.amount}`}</div> 
+        </div>
+      ))
+      }
+    </div>
+  )
+}
+
+//Child component 4 - Return the total amount of all the names
+const Total = (props) => {
+
+  const totalAmount = props.persons.reduce((allNames, person) => allNames + person.amount, 0);
+  
+  return (
+    <div className="Total">
+      <h3>Display the total amount of all the names</h3>
+      <div>Total amount of all the names: {totalAmount}</div>
+    </div>
+    
+  )
+}
+
+//Child component 5 - Return the amount of the name given as a parameter
+const Length = (props) => {
+
+  const arrayLength = props.persons.length;
+
+  return (
+     <div className="arrayLength">
+       <h3>Display the total amount of given inputs</h3>
+       <div>Total amount of inputs: {arrayLength}</div>
+     </div>
+  )
 }
 
 export default App;
